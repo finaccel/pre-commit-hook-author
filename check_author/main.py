91eb18c243b36de01885bac5d9d50310d357f4f2
roman <muhamad.rohman@finaccel.co>
2022-09-14 07:55:57 +0700

    chore(config): add linter config (#1)

--- check_author/main.py
+++ check_author/main.py
@@ -5,7 +5,7 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("filenames", nargs="*")
     args = parser.parse_args()
-        
+
     errors         = []
     is_author      = False
     is_description = False
@@ -14,34 +14,34 @@ def main():
         file_extension = pathlib.Path(filename).suffix
         if file_extension != ".py" and file_extension != ".sql":
             continue
-        
+
         count_lines = 0
-        with open(filename, 'rb') as fb:  
+        with open(filename, 'rb') as fb:
             for line in fb:
                 # check existing author
                 if 'author' in line.decode():
                     is_author = True
-                    
+
                 # check existing description
                 if 'description' in line.decode():
                     is_description = True
-                
+
                 if count_lines > 5:
                     break
-                
+
                 count_lines += 1
-        
+
         # file is empty
         if count_lines <= 2:
             errors.append(TypeError(f"{filename} is empty"))
             continue
-        
+
         if not is_author:
             errors.append(TypeError(f"{filename} has no author"))
-            
+
         if not is_description:
             errors.append(TypeError(f"{filename} has no description (description to let others know the purpose of this file)"))
-    
+
     if len(errors) > 0:
         return errors
     else:
